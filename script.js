f839edbb6a19eec7a606ef14baf18f4f66363165
Vijaya187 <vijayar929@gmail.com>
2024-08-02 16:11:30 +0530

    folder add

---
+++ script.js
@@ -0,0 +1,105 @@
+const sideMenu = document.querySelector("#sideMenu");
+
+function openMenu() {
+  sideMenu.style.transform = "translateX(-16rem)";
+}
+
+function closeMenu() {
+  sideMenu.style.transform = "translateX(16rem)";
+}
+
+// copy right year
+
+var currentYear = new Date().getFullYear();
+document.getElementById("year").textContent =
+currentYear + " " + "Vijay. All rights reserved";
+
+
+
+// scroll nav bar bg-color change
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop=blur-lg",
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.remove(
+      "bg-white",
+      "shadow-sm",
+      "bg-opacity-50",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
+    );
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop=blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.add(
+      "bg-white",
+      "shadow-sm",
+      "bg-opacity-50",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
+    );
+  }
+});
+
+// darkmode and lightmode
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+}
+
+
+// Form submit alert message and reset input
+
+document.getElementById("myForm").addEventListener('submit', function(e){
+  e.preventDefault();
+  Swal.fire({
+    title: "Custom animation with Animate.css",
+    showClass: {
+      popup: `
+      animate__animated
+      animate__fadeInUp
+      animate__faster
+    `,
+    },
+    hideClass: {
+      popup: `
+      animate__animated
+      animate__fadeOutDown
+      animate__faster
+    `,
+    },
+  });
+
+  this.reset();
+});
